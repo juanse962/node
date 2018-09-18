cfa65d233f8356c9caf4ab80fd50861ac4f7eabc
Juan Gomez <gomex086@gmail.com>
2018-09-17 23:20:50 -0500

    Escritura de archivos

---
+++ 03-bases-de-node/app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let base = 5;
+let data = '';
+
+for (let index = 1; index <= 10; index++) {
+    data += `${base} * ${index} = ${base*index} \n`;
+
+}
+
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`El archivo de la tabla del ${base} ha sido creado`);
+});
