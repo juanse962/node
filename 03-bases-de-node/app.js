69f5debb32746e8c436a85b99452b279e3641557
Juan Gomez <gomex086@gmail.com>
2018-09-18 21:30:51 -0500

    Agregando package.json

--- 03-bases-de-node/app.js
+++ 03-bases-de-node/app.js
@@ -1,13 +1,31 @@
-const fs = require('fs');
+const argv = require('yargs').argv;
 
-let base = 5;
-let data = '';
+const { crearArchivo } = require('./multiplicar/multiplicar');
 
-for (let index = 1; index <= 10; index++) {
-    data += `${base} * ${index} = ${base*index} \n`;
-}
+// let base = '5';
 
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log(`El archivo de la tabla del ${base} ha sido creado`);
-});
+// console.log(process.argv);
+let argv2 = process.argv;
+console.log(argv);
+console.log(argv2);
+// let parametro = argv[2];
+// let base = parametro.split(`=`)[1];
+
+// console.log(base);
+
+
+// let data = '';
+
+// for (let index = 1; index <= 10; index++) {
+//     data += `${base} * ${index} = ${base*index} \n`;
+
+// }
+
+// fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+//     if (err) throw err;
+//     console.log(`El archivo de la tabla del ${base} ha sido creado`);
+// });
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${archivo}`))
+//     .catch(e => console.log(e));
